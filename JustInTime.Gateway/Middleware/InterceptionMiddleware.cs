d58b1caa98d1f3cd74927a1eb414231292f199e1
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-10-28 22:22:23 -0300

    feat: add restrict access middleware

---
+++ JustInTime.Gateway/Middleware/InterceptionMiddleware.cs
@@ -0,0 +1,10 @@
+﻿namespace JustInTime.Gateway.Middleware;
+
+public class InterceptionMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Request.Headers["Referrer"] = "Api-Gateway";
+        await next(context);
+    }
+}
--- JustInTime.Gateway/Program.cs
+++ JustInTime.Gateway/Program.cs
@@ -1,25 +1,30 @@
+using JustInTime.Gateway.Middleware;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+        });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseCors();
 app.UseHttpsRedirection();
 
+app.UseMiddleware<InterceptionMiddleware>();
+
 app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseOcelot().Wait();
 app.Run();
