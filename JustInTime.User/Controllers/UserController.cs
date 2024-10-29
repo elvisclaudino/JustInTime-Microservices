9e808d810edc9c168ac0bc249a5b00f5a415a2b1
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-10-28 22:30:39 -0300

    feat: add user

---
+++ JustInTime.User/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustInTime.User.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private static readonly string[] Users = new[]
+    {
+        "Netcode", "Hub", "Fred"
+    };
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(Users);
+    }
+}
