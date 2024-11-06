982a3a708039c6bf94644f4f6109e572e49c40f4
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-11-05 22:03:41 -0300

    feat: add requested culture

--- JustInTime.User/Program.cs
+++ JustInTime.User/Program.cs
@@ -1,3 +1,4 @@
+using JustInTime.User.Middleware;
 using SharedLibrary;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
