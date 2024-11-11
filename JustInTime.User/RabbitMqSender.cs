0bb67092d56f68f1776607ebcd51fee2778180fc
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-11-11 20:46:39 -0300

    feat: add rabbit mq sender and receiver

---
+++ JustInTime.User/RabbitMqSender.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace JustInTime.User;
+
+public class RabbitMqSender
+{
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public RabbitMqSender(string hostname = "localhost")
+    {
+        var factory = new ConnectionFactory() { HostName = hostname };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: "user_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    }
+
+    public void SendMessage(string message)
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+        _channel.BasicPublish(exchange: "", routingKey: "user_queue", basicProperties: null, body: body);
+        Console.WriteLine($"[x] Sent {message}");
+    }
+
+    public void Close() => _connection.Close();
+}
